4fb4675b79570cb8d84126593e9a5e1879a299c6
ewilso <17hughwilson@gmail.com>
2020-11-05 16:46:38 +0000

    Latest builds

--- .config/fish/conf.d/omf.fish
+++ .config/fish/conf.d/omf.fish
@@ -12,5 +12,5 @@ alias orphan='sudo pacman -Rns (pacman -Qtdq)'
 alias checkluke="cd ~/lib/video/'Luke Smith' && youtube-dl --download-archive .downloads -o '%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/channel/UC2eYFnH61tmytImy1mTYvhA"
 alias checkdistro="cd  ~/lib/video/DistroTube && youtube-dl --download-archive .downloads -o '%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/channel/UCVls1GmFKf6WlTraIb_IaJg"
 alias reset='cd & /bin/clear && fish'
-alias ls='exa -alr --color always'
+alias ls='exa -lah --color always'
 export PATH="/home/wilson/.cargo/bin/:$PATH"
---
+++ .config/spotify/Users/awesomemixer13-user/frecency.pb
@@ -0,0 +1,10 @@
+
+O
+8spotify:user:sonymusicuk:playlist:0LjCLELG5F2oXW1L8InF7i	W@^Hw@ ­áïù
+R
+;spotify:user:awesomemixer13:playlist:5iwp3HBI5fJJ0o4spIx4ZI	
+Ø>„¢w@ ‚»“û
+R
+;spotify:user:awesomemixer13:playlist:6yRC8PtDpUoPOzpUz8Jrmx	ˆlvßw@ …Şïù
+R
+;spotify:user:awesomemixer13:playlist:29s3N6HAt0qysU74UYU1lo	˜‰.Nş’w@ ¦ Ÿú
