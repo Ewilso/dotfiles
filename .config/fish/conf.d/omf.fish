75e0dc783d899ae3f31bd8a9ead79a864898f59a
ewilso <17hughwilson@gmail.com>
2020-12-07 18:53:27 +0000

    New figs

--- .config/fish/conf.d/omf.fish
+++ .config/fish/conf.d/omf.fish
@@ -6,11 +6,12 @@ set -q XDG_DATA_HOME
 # Load Oh My Fish configuration.
 
 alias dl='cd /home/wilson/lib/scripts && ./downloader'
-alias night='xrandr --output LVDS-1 --brightness 0.4'
+alias night='xrandr --output eDP1 --brightness 0.4'
 alias vimclear='cd ~/ && ls -a | grep .viminf | xargs -r rm '
 alias orphan='sudo pacman -Rns (pacman -Qtdq)'
 alias checkluke="cd ~/lib/video/'Luke Smith' && youtube-dl --download-archive .downloads -o '%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/channel/UC2eYFnH61tmytImy1mTYvhA"
 alias checkdistro="cd  ~/lib/video/DistroTube && youtube-dl --download-archive .downloads -o '%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/channel/UCVls1GmFKf6WlTraIb_IaJg"
 alias reset='cd & /bin/clear && fish'
 alias ls='exa -lah --color always'
+alias powertop='sudo powertop'
 export PATH="/home/wilson/.cargo/bin/:$PATH"
