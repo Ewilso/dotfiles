d4cfa162cdf0e896135909dfe9f4d1ebe8f0e867
ewilso <17hughwilson@gmail.com>
2021-04-09 17:12:00 +0100

    New stuff

---
+++ Aperture/fish/conf.d/omf.fish
@@ -0,0 +1,18 @@
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+# Load Oh My Fish configuration.
+
+alias dl='cd /home/wilson/lib/scripts && ./downloader'
+alias night='xrandr --output eDP1 --brightness 0.4'
+alias vimclear='cd ~/ && ls -a | grep .viminf | xargs -r rm '
+alias orphan='sudo pacman -Rns (pacman -Qtdq)'
+alias checkluke="cd ~/lib/video/'Luke Smith' && youtube-dl --download-archive .downloads -o '%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/channel/UC2eYFnH61tmytImy1mTYvhA"
+alias checkdistro="cd  ~/lib/video/DistroTube && youtube-dl --download-archive .downloads -o '%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/channel/UCVls1GmFKf6WlTraIb_IaJg"
+alias reset='cd & /bin/clear && fish'
+alias ls='exa -lah --color always'
+alias powertop='sudo powertop'
+export PATH="/home/wilson/.cargo/bin/:$PATH"
+export PATH="/home/wilson/.local/bin/:$PATH"
